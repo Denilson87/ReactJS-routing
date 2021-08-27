f84006882b820db60daa02415c0c25642b6602c2
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-08-27 04:20:42 -0700

    changing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,7 +61,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/products'
+                to=''
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
@@ -71,7 +71,7 @@ function Navbar() {
 
             <li>
               <Link
-                to='/sign-up'
+                to=''
                 className='nav-links-mobile'
                 onClick={closeMobileMenu}
               >
