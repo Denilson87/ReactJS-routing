555e55a1c8181af2e4f7bdf23ecc393981288f07
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-08-27 03:30:33 -0700

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
+          Visite a cidade de xai-xai
         </p>
         <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
+          Registe-se e receba novidades da cidade do xai-xai
         </p>
         <div className='input-areas'>
           <form>
@@ -21,38 +21,38 @@ function Footer() {
               type='email'
               placeholder='Your Email'
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            <Button buttonStyle='btn--outline'>subscreva-se</Button>
           </form>
         </div>
       </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+            <h2>Sobre xai-xai</h2>
+            <Link to='/sign-up'>Regista-se</Link>
+            <Link to='/'>Cultura</Link>
+            <Link to='/'>Praias</Link>
+            <Link to='/'>Destinos</Link>
+            <Link to='/'>Hoteis</Link>
           </div>
           <div class='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <h2>Saiba mais</h2>
+            <Link to='/'>Contactos</Link>
+            <Link to='/'>Email</Link>
+            <Link to='/'>Galerias</Link>
+            <Link to='/'>Portifolio</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <Link to='/'></Link>
+            <Link to='/'>Praias</Link>
+            <Link to='/'>Hoteis</Link>
+            <Link to='/'>Aventura</Link>
           </div>
           <div class='footer-link-items'>
-            <h2>Social Media</h2>
+            <h2>Redes socias</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
