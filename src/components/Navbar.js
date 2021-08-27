4204eea8a46d3e5b31594b988328cbcc2ab8bf93
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-08-27 03:17:54 -0700

    updating navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,7 +79,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+          {button && <Button buttonStyle='btn--outline'>Registar</Button>}
         </div>
       </nav>
     </>
