63ae53749b1a03d7e2bb8a016aebeaa87f56f4f9
Denilson Mucavele <77127716+Denilson87@users.noreply.github.com>
2021-08-27 04:17:46 -0700

    updating beach

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -10,7 +10,7 @@ import Cards from '../Cards';
 function Services() {
   return (
     <>
-    <h1 className='services'>SERVICES</h1>;
+    <h1 className='services'>Praias e Turismo</h1>;
       <Cards />     
     </>
   );
